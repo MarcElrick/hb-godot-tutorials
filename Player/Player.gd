c2ef12e88f46ce4334c2a1106d8807ca9e2a50f9
Marc Elrick <marc.elrick@fanduel.com>
2021-06-18 15:12:49 +0100

    first commit

---
+++ Player/Player.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 80
+const ACCELERATION = 500
+const FRICTION = 750
+var velocity = Vector2.ZERO
+
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+
+	if input_vector != Vector2.ZERO:
+		animationState.travel("Run")
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
