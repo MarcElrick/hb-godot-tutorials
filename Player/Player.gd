2e2cdaee88e01d28d8eb738d0ebcf2f2acf3866f
Marc Elrick <marc.elrick@fanduel.com>
2021-06-18 17:27:38 +0100

    Further progress

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,11 +6,37 @@ const FRICTION = 750
 var velocity = Vector2.ZERO
 
 
+enum {
+	MOVE, ROLL, ATTACK
+}
+var state = MOVE
+
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+
+
+func _ready():
+	animationTree.active = true
+
+	
 func _physics_process(delta):
+	match state:
+		MOVE:	
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+		ROLL:
+			pass
+	
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -20,6 +46,7 @@ func _physics_process(delta):
 		animationState.travel("Run")
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -27,3 +54,10 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+		
+
+func attack_animation_finished():
+	state = MOVE
