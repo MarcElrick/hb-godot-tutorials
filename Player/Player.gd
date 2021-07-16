08c242e1303b809b98b908474d67c90527b2c51e
Marc Elrick <32804604+MarcElrick@users.noreply.github.com>
2021-07-16 10:49:12 +0100

    Week 2

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,13 +3,18 @@ extends KinematicBody2D
 const MAX_SPEED = 80
 const ACCELERATION = 500
 const FRICTION = 750
+const ROLL_SPEED = 125
 var velocity = Vector2.ZERO
+
+
+
 
 
 enum {
 	MOVE, ROLL, ATTACK
 }
 var state = MOVE
+var roll_vector = Vector2.DOWN
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -19,7 +24,6 @@ onready var animationState = animationTree.get("parameters/playback")
 
 func _ready():
 	animationTree.active = true
-
 	
 func _physics_process(delta):
 	match state:
@@ -28,12 +32,17 @@ func _physics_process(delta):
 		ATTACK:
 			attack_state(delta)
 		ROLL:
-			pass
+			roll_state(delta)
 	
 
-func attack_state(delta):
+func attack_state(_delta):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
+
+func roll_state(delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel('Roll')
+	move()
 
 
 func move_state(delta):
@@ -43,21 +52,37 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		animationState.travel("Run")
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	velocity = move_and_slide(velocity)
+	move()
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 		
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
+
+
+func move():
+	velocity = move_and_slide(velocity)
+	
+func roll_animation_finished():
+	state = MOVE
+	velocity = velocity/2
 
 func attack_animation_finished():
 	state = MOVE
+
+
+func _on_Hitbox_area_entered(area):
+	pass # Replace with function body.
